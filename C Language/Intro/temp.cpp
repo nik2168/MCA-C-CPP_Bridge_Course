486690bad59a5314eecbf78cbf59e1ec10d05ed7
nik2168 <128375663+nik2168@users.noreply.github.com>
2024-10-18 20:43:46 +0530

    added java and c programs in this new repo

---
+++ C Language/Intro/temp.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int sum(int a, int b){
+    return a + b;
+}
+
+int main(){
+
+int a = 9;
+int b = 9;
+// int sum(int, int);
+
+int r = sum(a, b);
+cout<<r<<endl;
+    return 0;
+}
+
